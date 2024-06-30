a6fbd47ffa3278f3bdcc84b6dcade930692afde2
abuinu <achankai@proton.me>
2024-06-30 21:29:14 +0800

    add iam resources

---
+++ services/github-action-access-state-backend/outputs.tf
@@ -0,0 +1,8 @@
+output "iam_user_access_key_id" {
+  value = aws_iam_user.terraform_state_backend.id
+}
+
+output "iam_user_secret_access_key" {
+  value     = aws_iam_access_key.terraform_state_backend.secret
+  sensitive = true
+}
